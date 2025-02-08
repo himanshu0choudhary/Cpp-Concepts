89e9830836e6ad76e36501796e5071d78e8f2431
Himanshu Choudhary <54780264+himanshu0choudhary@users.noreply.github.com>
2025-02-08 23:39:47 +0530

    Add files via upload

---
+++ Locks.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void unique_lock_thread(){
+    mutex m;
+
+    // Has ownership of mutex
+    // Locks automatically and unlocks automatically when destroyed
+    // Can be manually locked and unlocked
+    // movable not copyable
+    // Supports try_lock
+    unique_lock<mutex> lock(m);
+
+    // Do something
+    cout<<"unique_lock_thread"<<endl;
+}
+
+void lock_guard_thread(){
+    mutex m;
+
+    // Does not own the mutex
+    // When std::lock_guard is created, it locks the mutex
+    // If an exception occurs, std::lock_guard ensures that the mutex is released
+    // Cannot be copied or moved
+    lock_guard<mutex> lock(m);
+
+    // Do something
+    cout<<"lock_guard_thread"<<endl;
+}
+
+int main() {
+    thread t1(unique_lock_thread);
+
+    thread t2(lock_guard_thread);
+}
